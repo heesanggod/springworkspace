d5ca712a3d25afcbadddfb95ca20035b0899a352
parksungeon <un109101@naver.com>
2024-03-18 16:48:36 +0900

    2024.03.18 4:50

--- gradle/app/src/main/java/springworkspace/App.java
+++ gradle/app/src/main/java/springworkspace/App.java
@@ -3,10 +3,23 @@
  */
 package springworkspace;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+// @NoArgsConstructor // ?
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Builder
 class Person {
+
+    private final String name;
+    private int age;
 
 }
 
@@ -17,5 +30,11 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        Person person = new Person("홍길동", 20);
+        person = new Person("고길동");
+
+        person = Person.builder().name("홍길동").age(20).build();
+
     }
 }
